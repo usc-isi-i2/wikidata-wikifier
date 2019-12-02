119c39ac7de5ca4cd5964fc44abf8f95966a10fa
saggu <amandeep.s.saggu@gmail.com>
2019-12-02 11:56:19 -0800

    no headers for certain formats

--- wikifier_service.py
+++ wikifier_service.py
@@ -22,12 +22,16 @@ def wikify():
     df.fillna('', inplace=True)
     _uuid_hex = uuid4().hex
     columns = request.form.get('columns')
+    format = request.form.get('format')
     _path = 'user_files/{}_{}'.format(columns, _uuid_hex)
     pathlib.Path(_path).mkdir(parents=True, exist_ok=True)
     df.to_csv('{}/input.csv'.format(_path), index=False)
 
-    r_df = wikifier.wikify(df, column=columns)
-    r_df.to_csv('{}/results.csv'.format(_path), index=False)
+    r_df = wikifier.wikify(df, column=columns, format=format)
+    if format == 'wikifier' or format == 'iswc':
+        r_df.to_csv('{}/results.csv'.format(_path), index=False, header=False)
+    else:
+        r_df.to_csv('{}/results.csv'.format(_path), index=False)
     return send_from_directory(_path, 'results.csv')
 
 
