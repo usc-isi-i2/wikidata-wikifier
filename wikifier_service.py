323b80a87ae34adb3a1f6fb01b9d6542cc250e1f
saggu <amandeep.s.saggu@gmail.com>
2021-03-22 18:11:43 -0700

    top k results

--- wikifier_service.py
+++ wikifier_service.py
@@ -22,27 +22,19 @@ def wikidata_wikifier():
 def wikify():
     columns = request.form.get('columns', None)
 
+    k = int(request.form.get('k', 1))
+
     df = pd.read_csv(request.files['file'], dtype=object)
 
     df.fillna('', inplace=True)
     _uuid_hex = uuid4().hex
 
-    # format = request.form.get('format', 'wikifier')
     _path = 'user_files/{}_{}'.format(columns, _uuid_hex)
     pathlib.Path(_path).mkdir(parents=True, exist_ok=True)
-    # df.to_csv('{}/input.csv'.format(_path), index=False, header=None)
 
-    r_df = wikifier.wikify(df, columns, debug=True)
+    r_df = wikifier.wikify(df, columns, debug=True, k=k)
     r_df.to_csv('{}/results.csv'.format(_path), index=False)
     return send_from_directory(_path, 'results.csv')
-    # if format and (format.lower() == 'wikifier' or format.lower() == 'iswc'):
-    #     r_df.to_csv('{}/results.csv'.format(_path), index=False, header=False)
-    #     lines = open('{}/results.csv'.format(_path)).readlines()
-    #     lines = [line.replace('\n', '') for line in lines]
-    #     return json.dumps({'data': lines})
-    # else:
-    #     r_df.to_csv('{}/results.csv'.format(_path), index=False)
-    #     return send_from_directory(_path, 'results.csv')
 
 
 if __name__ == '__main__':
