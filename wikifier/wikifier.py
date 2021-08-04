a86604711208dee95989c8f414b8d52c68b66ff6
saggu <amandeep.s.saggu@gmail.com>
2021-08-04 15:38:14 -0700

    add nih option

--- wikifier/wikifier.py
+++ wikifier/wikifier.py
@@ -31,6 +31,9 @@ class Wikifier(object):
         self.model_path = _model_path if _model_path else config['model_path']
 
         _min_max_scaler_path = os.environ.get('WIKIFIER_MIN_MAX_SCALER_PATH', None)
+
+        _custom_context_path = os.environ.get('CUSTOM_CONTEXT_PATH', None)
+        self.custom_context_path = _custom_context_path if _custom_context_path else config['custom_context_path']
         self.min_max_scaler_path = _min_max_scaler_path if _min_max_scaler_path else config["min_max_scaler_path"]
         self.features = ['pagerank', 'retrieval_score', 'monge_elkan', 'monge_elkan_aliases', 'des_cont_jaccard',
                          'jaro_winkler', 'levenshtein', 'singleton', 'num_char', 'num_tokens',
@@ -39,10 +42,15 @@ class Wikifier(object):
         self.classifier_features = ["aligned_pagerank", "smallest_qnode_number", "monge_elkan",
                                     "des_cont_jaccard_normalized"]
 
-    def wikify(self, i_df: pd.DataFrame, columns: str, output_path: str, debug: bool = False, k: int = 1,
-               colorized_output: bool = False) -> str:
+    def wikify(self,
+               i_df: pd.DataFrame,
+               columns: str,
+               output_path: str,
+               debug: bool = False,
+               k: int = 1,
+               colorized_output: bool = False,
+               high_precision: bool = False) -> str:
         temp_dir = tempfile.mkdtemp()
-
         pipeline_temp_dir = f"{temp_dir}/temp"
         input_file_path = f"{temp_dir}/input.csv"
         candidate_file_path = f"{temp_dir}/candidates.csv"
@@ -92,11 +100,14 @@ class Wikifier(object):
         # feature computation
         features_str = ",".join(self.features)
         feature_computation_command = f"tl align-page-rank {candidate_file_path} \
-                                        / string-similarity -i --method symmetric_monge_elkan:tokenizer=word -o monge_elkan \
-                                        / string-similarity -i --method symmetric_monge_elkan:tokenizer=word -c label_clean kg_aliases -o monge_elkan_aliases \
+                                        / string-similarity -i --method symmetric_monge_elkan:tokenizer=word \
+                                        -o monge_elkan \
+                                        / string-similarity -i --method symmetric_monge_elkan:tokenizer=word \
+                                        -c label_clean kg_aliases -o monge_elkan_aliases \
                                         / string-similarity -i --method jaro_winkler -o jaro_winkler \
                                         / string-similarity -i --method levenshtein -o levenshtein \
-                                        / string-similarity -i --method jaccard:tokenizer=word -c kg_descriptions context -o des_cont_jaccard \
+                                        / string-similarity -i --method jaccard:tokenizer=word -c kg_descriptions \
+                                        context -o des_cont_jaccard \
                                         / normalize-scores -c des_cont_jaccard / smallest-qnode-number \
                                         / mosaic-features -c kg_labels --num-char --num-tokens \
                                         / create-singleton-feature -o singleton \
@@ -131,6 +142,52 @@ class Wikifier(object):
                                         --ranking-model {self.model_path} \
                                         --features {features_str} \
                                         --normalization-factor {self.min_max_scaler_path} \
+                                        > {intermediate_file}" if not high_precision else \
+            f"tl align-page-rank {candidate_file_path} \
+                                        / string-similarity -i --method symmetric_monge_elkan:tokenizer=word \
+                                        -o monge_elkan \
+                                        / string-similarity -i --method symmetric_monge_elkan:tokenizer=word \
+                                        -c label_clean kg_aliases -o monge_elkan_aliases \
+                                        / string-similarity -i --method jaro_winkler -o jaro_winkler \
+                                        / string-similarity -i --method levenshtein -o levenshtein \
+                                        / string-similarity -i --method jaccard:tokenizer=word -c kg_descriptions \
+                                        context -o des_cont_jaccard \
+                                        / normalize-scores -c des_cont_jaccard / smallest-qnode-number \
+                                        / mosaic-features -c kg_labels --num-char --num-tokens \
+                                        / create-singleton-feature -o singleton \
+                                        / vote-by-classifier  \
+                                        --prob-threshold 0.995 \
+                                        --model {self.classifier_model_path} \
+                                        --features {','.join(self.classifier_features)} \
+                                        / score-using-embedding \
+                                        --column-vector-strategy centroid-of-lof \
+                                        --lof-strategy ems-mv \
+                                        -o lof-graph-embedding-score \
+                                        --embedding-file {graph_embedding_complex_file} \
+                                        --embedding-url {self.es_url}/{self.augmented_dwd_index}/ \
+                                        / generate-reciprocal-rank  \
+                                        -c lof-graph-embedding-score \
+                                        -o lof-reciprocal-rank \
+                                        / compute-tf-idf \
+                                        --feature-file {class_count_file} \
+                                        --feature-name class_count \
+                                        --singleton-column is_lof \
+                                        -o lof_class_count_tf_idf_score \
+                                        / compute-tf-idf \
+                                        --feature-file {property_count_file} \
+                                        --feature-name property_count \
+                                        --singleton-column is_lof \
+                                        -o lof_property_count_tf_idf_score \
+                                        / context-match \
+                                        --context-file {context_file}  \
+                                        --custom-context-file {self.custom_context_path} \
+                                        --string-separator ';' \
+                                        -o context_score \
+                                        --debug \
+                                        / predict-using-model -o siamese_prediction \
+                                        --ranking-model {self.model_path} \
+                                        --features {features_str} \
+                                        --normalization-factor {self.min_max_scaler_path} \
                                         > {intermediate_file}"
 
         fc_output = subprocess.getoutput(feature_computation_command)
@@ -143,11 +200,13 @@ class Wikifier(object):
                              / add-color -c siamese_prediction -k {k} \
                              --output {output_file}"
         else:
-            output_command = f"tl get-kg-links -c siamese_prediction -k {k} {intermediate_file} \
-                               / join -c ranking_score -f {input_file_path} --extra-info \
+            output_command = f"tl get-kg-links -c siamese_prediction -k {k} --k-rows {intermediate_file} \
+                               / join -c siamese_prediction -f {input_file_path} --extra-info \
                                > {output_file}"
         oo_output = subprocess.getoutput(output_command)
-        print(oo_output)
+
+        if debug:
+            print(oo_output)
 
         copy_command = f"cp {output_file} {output_path}"
         subprocess.getoutput(copy_command)
