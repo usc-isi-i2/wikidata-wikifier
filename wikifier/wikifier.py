9fff04a19d708e7b0a7af7b56720e4ee2c88b85b
saggu <amandeep.s.saggu@gmail.com>
2021-10-07 15:19:46 -0700

    new pipeline

--- wikifier/wikifier.py
+++ wikifier/wikifier.py
@@ -28,19 +28,22 @@ class Wikifier(object):
 
         _min_max_scaler_path = os.environ.get('WIKIFIER_MIN_MAX_SCALER_PATH', None)
 
-        _custom_context_path = os.environ.get('CUSTOM_CONTEXT_PATH', '')
-        self.custom_context_path = _custom_context_path if _custom_context_path else config['custom_context_path']
         self.min_max_scaler_path = _min_max_scaler_path if _min_max_scaler_path else config["min_max_scaler_path"]
-        self.features = [
-            "monge_elkan", "monge_elkan_aliases", "jaro_winkler",
-            "levenshtein", "singleton", "context_score_3",
-            "pgt_centroid_score", "pgt_class_count_tf_idf_score",
-            "pgt_property_count_tf_idf_score"
-        ]
-        self.pseudo_gt_features = ["monge_elkan", "monge_elkan_aliases", "jaro_winkler", "levenshtein", "singleton",
-                                   "pgr_rts", "context_score", "smc_class_score", "smc_property_score"]
-        self.pseudo_gt_model = config['pseudo_gt_model']
-        self.pseudo_gt_min_max_scaler_path = config['pseudo_gt_min_max_scaler_path']
+        self.features = ["monge_elkan", "monge_elkan_aliases", "jaro_winkler",
+                         "levenshtein", "singleton", "context_score_3", "pgt_centroid_score",
+                         "pgt_class_count_tf_idf_score",
+                         "pgt_property_count_tf_idf_score", "num_occurences"]
+
+        self.pseudo_gt_features = ["monge_elkan", "monge_elkan_aliases", "jaro_winkler",
+                                   "levenshtein", "singleton", "pgr_rts", "context_score",
+                                   "smc_class_score", "smc_property_score"]
+        _pseudo_gt_model = os.environ.get('PSEUDO_GT_MODEL', None)
+        self.pseudo_gt_model = _pseudo_gt_model if _pseudo_gt_model else config['pseudo_gt_model']
+
+        _pseudo_g_min_max_scaler_path = os.environ.get('PSEUDO_GT_MIN_MAX_SCALER_PATH', None)
+
+        self.pseudo_gt_min_max_scaler_path = _pseudo_g_min_max_scaler_path if _pseudo_g_min_max_scaler_path else config[
+            'pseudo_gt_min_max_scaler_path']
 
     def wikify(self,
                i_df: pd.DataFrame,
@@ -48,10 +51,12 @@ class Wikifier(object):
                output_path: str,
                debug: bool = False,
                k: int = 1,
-               colorized_output: bool = False,
-               high_precision: bool = False) -> str:
+               colorized_output: bool = False) -> str:
         temp_dir = tempfile.mkdtemp()
-        print(temp_dir)
+        
+        aux_path = f"{temp_dir}/aux_files"
+        Path(aux_path).mkdir(parents=True, exist_ok=True)
+
         pipeline_temp_dir = f"{temp_dir}/temp"
         input_file_path = f"{temp_dir}/input.csv"
         candidate_file_path = f"{temp_dir}/candidates.csv"
@@ -60,14 +65,11 @@ class Wikifier(object):
 
         Path(pipeline_temp_dir).mkdir(parents=True, exist_ok=True)
 
-        graph_embedding_complex_file = f"{pipeline_temp_dir}/graph_embedding_complex.tsv"
-        class_count_file = f"{pipeline_temp_dir}/class_count.tsv"
-        property_count_file = f"{pipeline_temp_dir}/property_count.tsv"
-        context_file = f"{pipeline_temp_dir}/context.tsv"
-        context_property_file = f"{pipeline_temp_dir}/context_property.csv"
-
-        if self.custom_context_path == '' or (not os.path.exists(self.custom_context_path)):
-            high_precision = False
+        graph_embedding_complex_file = f"{aux_path}/graph_embedding_complex.tsv"
+        class_count_file = f"{aux_path}/class_count.tsv"
+        property_count_file = f"{aux_path}/property_count.tsv"
+        context_file = f"{aux_path}/context.jl"
+        context_property_file = f"{aux_path}/context_property.csv"
 
         i_df.to_csv(input_file_path, index=False)
 
@@ -92,30 +94,36 @@ class Wikifier(object):
         if debug:
             print(cc_output)
 
-        column_rename_dict = {
-            'graph_embedding_complex': 'embedding',
-            'class_count': 'class_count',
-            'property_count': 'property_count',
-            'context': 'context'
-        }
         for field in self.auxiliary_fields.split(','):
             aux_list = []
-            for f in glob(f'{pipeline_temp_dir}/*{field}.tsv'):
-                aux_list.append(pd.read_csv(f, sep='\t'))
-            aux_df = pd.concat(aux_list).drop_duplicates(subset=['qnode']).rename(
-                columns={field: column_rename_dict[field]})
-            aux_df.to_csv(f'{pipeline_temp_dir}/{field}.tsv', sep='\t', index=False)
+            if field == 'context':
+                file_list = glob(f'{pipeline_temp_dir}/*{field}.jl')
+                o_f = open(context_file, 'w')
+
+                for i_f_P in file_list:
+                    i_f = open(i_f_P)
+                    for line in i_f:
+                        o_f.write(line)
+                    i_f.close()
+                o_f.close()
+
+            else:
+                for f in glob(f'{pipeline_temp_dir}/*{field}.tsv'):
+                    aux_list.append(pd.read_csv(f, sep='\t', dtype=object))
+                aux_df = pd.concat(aux_list).drop_duplicates(subset=['qnode'])
+                if field == 'class_count':
+                    aux_df.to_csv(class_count_file, sep='\t', index=False)
+                elif field == 'property_count':
+                    aux_df.to_csv(property_count_file, sep='\t', index=False)
+                else:
+                    aux_df.to_csv(graph_embedding_complex_file, sep='\t', index=False)
 
         # feature computation
         pgt_features_str = ",".join(self.pseudo_gt_features)
         features_str = ",".join(self.features)
-        print(context_file)
-        print(class_count_file)
-        print(property_count_file)
-        print(pgt_features_str)
-        print(self.pseudo_gt_model)
-        print(self.pseudo_gt_min_max_scaler_path)
-        feature_computation_command = f"deduplicate-candidates -c kg_id {candidate_file_path} \
+        columns_to_color = f"{features_str},siamese_prediction"
+
+        feature_computation_command = f"tl deduplicate-candidates -c kg_id {candidate_file_path} \
                                         / string-similarity -i --method symmetric_monge_elkan:tokenizer=word -o \
                                         monge_elkan --threshold 0.5 \
                                         / string-similarity -i --method symmetric_monge_elkan:tokenizer=word \
@@ -186,7 +194,7 @@ class Wikifier(object):
         if colorized_output:
             output_command = f"tl get-kg-links -c siamese_prediction -k {k}  \
                              --k-rows  {intermediate_file}\
-                             / add-color -c siamese_prediction -k {k} \
+                             / add-color -c {columns_to_color} -k {k} \
                              --output {output_file}"
         else:
             output_command = f"tl get-kg-links -c siamese_prediction -k {k} --k-rows {intermediate_file} \
@@ -200,5 +208,5 @@ class Wikifier(object):
         copy_command = f"cp {output_file} {output_path}"
         subprocess.getoutput(copy_command)
 
-        # shutil.rmtree(temp_dir)
+        shutil.rmtree(temp_dir)
         return output_file.split("/")[-1]
