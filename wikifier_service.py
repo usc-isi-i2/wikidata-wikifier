2f0c155c645d7fcdd5df25e76abc4abf053aa0d2
GrantXie <67876263+GrantXie@users.noreply.github.com>
2021-10-10 06:58:56 -0700

    Add files via upload

--- wikifier_service.py
+++ wikifier_service.py
@@ -6,11 +6,122 @@ from flask import Flask
 from flask import request
 from flask import send_from_directory
 from wikifier.wikifier import Wikifier
+import requests
+import time
 
 app = Flask(__name__)
 
 wikifier = Wikifier()
 config = json.load(open('wikifier/config.json'))
+
+
+
+
+@app.route('/rec', methods=['POST','GET'])
+def rec():
+   
+    #print(request.headers)
+    #print(request.data)
+    #print(request.form)
+    #print(request.args)
+    #print(request.url)
+    #print(request.__dict__)
+    #print(request.referrer)
+    #if flask.request.method == 'POST':
+    #    print(flask.request)
+
+
+    #deal with callback requests for general info
+
+    query = request.form.get('queries')
+
+    if query == None:
+         query = request.args.get('queries')
+    
+    if query == None:
+        callback = request.args.get('callback', False)
+        if callback:
+            content = str(callback) + '(' + str({
+           "name" : "Table-Linker Reconciliation for OpenRefine (en)",
+           "identifierSpace" : "http://www.wikidata.org/entity/",
+           "schemaSpace" : "http://www.wikidata.org/prop/direct/",
+           "view" : {
+           "url" : "https://www.wikidata.org/wiki/{{id}}"
+           }}) + ')'
+            return content
+        else:
+            return {
+           "name" : "Wikidata Reconciliation for OpenRefine (en)",
+           "identifierSpace" : "http://www.wikidata.org/entity/",
+           "schemaSpace" : "http://www.wikidata.org/prop/direct/",
+           "view" : {
+           "url" : "https://www.wikidata.org/wiki/{{id}}"
+           }}
+    #deal with post/get queries         
+    else: 
+        k =3 
+        query = json.loads(query)
+
+        df = pd.DataFrame.from_dict(query, orient='index')
+
+
+        #df.to_csv('./test.csv')
+
+        label = []
+        for key in query.keys():
+            label.append(key)
+        df = df.reset_index(drop = True)
+        columns = 'query'
+
+        print(df)
+
+        if (len(df)) > 0 and 'properties' in df.columns:
+            for ele in (df['properties'][0]):
+                df[ele['pid']] = ''
+        
+            for i in range(0, len(df)):
+                ele = (df['properties'][i])
+                for col in ele:
+                    df[col['pid']][i] = col['v']
+            if 'type' in df.columns:
+                df = df.drop('type', 1)
+            df = df.drop('properties', 1)
+
+            if 'type_strict' in df.columns:
+                df = df.drop('type_strict', 1)
+
+
+        _uuid_hex = uuid4().hex
+
+        _path = 'user_files/{}_{}'.format(columns, _uuid_hex)
+        pathlib.Path(_path).mkdir(parents=True, exist_ok=True)
+
+        wikifier.wikify(df, columns, output_path=_path, debug=True, k=k,
+                                      colorized_output='test1.csv')
+
+        df = pd.read_excel(_path + '/colorized.xlsx')
+
+
+        output = {}
+        for ele in label:
+            output[ele] = {'result' : []}
+    
+        for i in range(0, len(df)):
+            output[label[df['row'][i]]]['result'].append({
+                "id" : df['kg_id'][i],
+                "name" : df['kg_labels'][i],
+                "type" : [{"id":"/qnode","name":"Qnode"}],
+                "score" : df['siamese_prediction'][i],
+                "match" : (float(df['siamese_prediction'][i])> 0.95 and int(df['rank'][i]) == 1)
+              })
+        
+        callback = request.args.get('callback', False)
+        if callback:
+            print(str(callback) + '(' + str(output) + ')')
+            return str(callback) + '(' + str(output) + ')'
+        else:
+            print(output)
+            return json.dumps(output)
 
 
 @app.route('/')
